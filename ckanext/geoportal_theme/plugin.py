8a2d21eccc6266cc307a7aec20210540f992e4fe
kino <ki3104@gmail.com>
2015-02-25 18:22:15 +0900

    add __init__.py, plugin.py

---
+++ ckanext/geoportal_theme/plugin.py
@@ -0,0 +1,17 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class GeoportalThemePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+#    plugins.implements(plugins.IRoutes, inherit=True)
+    # IConfigurer
+
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+        #toolkit.add_resource('public', 'ckanext-publicamundi')
+        toolkit.add_resource('fanstatic', 'geoportal_theme')
+#    def before_map(self, m):
+#        m.redirect('/', '/dataset')
+#        return m
